382574e365593844b28d11ef04e68b7dda4f9ae2
GhTmp1A <stanbutter4@gmail.com>
2022-11-03 14:01:35 -0400

    L4 tutorial

--- index.js
+++ index.js
@@ -1,7 +1,13 @@
-const DOMSELECTOR = {
+const DOMSelectors = {
   button: document.getElementById("btn"),
   text: document.querySelector("#text"),
-  point: document.querySelectorAll(".point"),
-  pointTwo: document.getElementById(".point2"),
+  box: document.getElementById("big-black-box"),
+  points: document.querySelectorAll(".point"),
 };
-console.log(DOMSELECTOR.pointTwo);
+function backgroundAndText(background, text) {
+  background.style.backgroundColor = "red";
+  text.textContent = "This is now a big bigger red box";
+  text.style.fontSize = "40px";
+}
+
+backgroundAndText(DOMSelectors.box, DOMSelectors.text);
