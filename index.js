79e4952afad6d7851b1c0bbcca8eb219c81c32f9
GhTmp1A <stanbutter4@gmail.com>
2022-11-09 13:59:52 -0500

    Create delete button and output div and start thinking of concept for project

--- index.js
+++ index.js
@@ -1,11 +1,29 @@
 const DOMSelectors = {
-  button: document.getElementById("send-btn"),
+  sendbutton: document.getElementById("send-btn"),
   box: document.getElementById("container-box"),
   input: document.querySelector("#input"),
+  clearbutton: document.getElementById("clear-btn"),
+  sendclearbutton: document.getElementById("sendclear-btn"),
+  deletebutton: document.getElementById("delete-btn"),
 };
 
-DOMSelectors.button.addEventListener("click", function () {
+DOMSelectors.sendbutton.addEventListener("click", function () {
+  let input = DOMSelectors.input.value;
+  DOMSelectors.box.insertAdjacentHTML("beforeend", `<p>${input}</p>`);
+});
+
+DOMSelectors.clearbutton.addEventListener("click", function () {
+  let input = DOMSelectors.input.remove;
+  DOMSelectors.box.removeAttribute("beforeend", `<p>${input}</p>`);
+  DOMSelectors.input.value = "";
+});
+
+DOMSelectors.sendclearbutton.addEventListener("click", function () {
   let input = DOMSelectors.input.value;
   DOMSelectors.box.insertAdjacentHTML("beforeend", `<p>${input}</p>`);
   DOMSelectors.input.value = "";
+});
+
+DOMSelectors.deletebutton.addEventListener("click", function () {
+  DOMSelectors.box.innerHTML = "Ba";
 });
