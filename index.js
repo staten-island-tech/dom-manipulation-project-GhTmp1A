776e68f11842eb5c7a0e1744f8378725502b0066
GhTmp1A <stanbutter4@gmail.com>
2022-11-10 14:01:29 -0500

    Progress on website. Make the rooms. Fix delete button to appear at card.

--- index.js
+++ index.js
@@ -1,6 +1,7 @@
 const DOMSelectors = {
   sendbutton: document.getElementById("send-btn"),
-  box: document.getElementById("container-box"),
+  containerbox: document.getElementById("container-box"),
+  outputbox: document.getElementById("output-box"),
   input: document.querySelector("#input"),
   clearbutton: document.getElementById("clear-btn"),
   sendclearbutton: document.getElementById("sendclear-btn"),
@@ -9,21 +10,23 @@ const DOMSelectors = {
 
 DOMSelectors.sendbutton.addEventListener("click", function () {
   let input = DOMSelectors.input.value;
-  DOMSelectors.box.insertAdjacentHTML("beforeend", `<p>${input}</p>`);
+  DOMSelectors.outputbox.insertAdjacentHTML("beforeend", `<p>${input}</p>`);
 });
 
 DOMSelectors.clearbutton.addEventListener("click", function () {
   let input = DOMSelectors.input.remove;
-  DOMSelectors.box.removeAttribute("beforeend", `<p>${input}</p>`);
+  DOMSelectors.outputbox.removeAttribute("beforeend", `<p>${input}</p>`);
   DOMSelectors.input.value = "";
 });
 
 DOMSelectors.sendclearbutton.addEventListener("click", function () {
   let input = DOMSelectors.input.value;
-  DOMSelectors.box.insertAdjacentHTML("beforeend", `<p>${input}</p>`);
+  DOMSelectors.outputbox.insertAdjacentHTML("beforeend", `<p>${input}</p>`);
   DOMSelectors.input.value = "";
 });
 
 DOMSelectors.deletebutton.addEventListener("click", function () {
-  DOMSelectors.box.innerHTML = "Ba";
+  let input = DOMSelectors.input.value;
+  DOMSelectors.outputbox.insertAdjacentHTML("beforeend", `<p>${input}</p>`);
+  DOMSelectors.outputbox.innerHTML = "";
 });
